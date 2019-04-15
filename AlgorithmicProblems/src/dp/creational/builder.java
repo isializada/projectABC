b9320d14c3032a859187a63828412c6f1985cd48
Ismayil Alizada <ismayil.alizada@diconium.com>
2019-04-15 18:39:36 +0200

    PROJECT ABC | Builder and singleton impl

--- AlgorithmicProblems/src/dp/creational/builder.java
+++ AlgorithmicProblems/src/dp/creational/builder.java
@@ -1,5 +1,90 @@
 package dp.creational;
 
+/*While Considering the builder pattern you need to look weather the object is having
+
+Complex constructor.
+Multiple constructor having combinations of multiple parameter with nested objects
+
+Large number of parameters.
+having large number of field parameter is also the key point to consider.
+
+Immutability.
+You can force the immutability to the object once you are done with creation of object.
+*/
+
 public class builder {
 
+  Person person = new Person.PersonBuilder()
+      .setAge(15)
+      .setFirstName("Ismayil")
+      .setLastName("Alizad")
+      .build();
+
+}
+
+class Person {
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private int age;
+  private double height;
+  private double weight;
+
+  Person(String firstName, String middleName, String lastName, int age, double height,
+      double weight) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+
+
+  static class PersonBuilder {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int age;
+    private double height;
+    private double weight;
+
+    public PersonBuilder() {
+    }
+
+    public PersonBuilder setFirstName(String firstName) {
+      this.firstName = firstName;
+      return this;
+    }
+
+    public PersonBuilder setMiddleName(String middleName) {
+      this.middleName = middleName;
+      return this;
+    }
+
+    public PersonBuilder setLastName(String lastName) {
+      this.lastName = lastName;
+      return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+      this.age = age;
+      return this;
+    }
+
+    public PersonBuilder setHeight(double height) {
+      this.height = height;
+      return this;
+    }
+
+    public PersonBuilder setWeight(double weight) {
+      this.weight = weight;
+      return this;
+    }
+
+    public Person build() {
+      return new Person(firstName, middleName, lastName, age, height, weight);
+    }
+  }
 }
--- AlgorithmicProblems/src/dp/creational/singleton.java
+++ AlgorithmicProblems/src/dp/creational/singleton.java
@@ -1,5 +1,22 @@
 package dp.creational;
 
 public class singleton {
+  public static void main(String[] args){
+    SingletonService instance1 = SingletonService.getInstance();
+    SingletonService instance2 = SingletonService.getInstance();
+    System.out.println(instance1.hashCode());
+    System.out.println(instance2.hashCode());
+  }
+}
 
+class SingletonService{
+  static SingletonService singletonService = null;
+
+  static SingletonService getInstance(){
+    if(null == singletonService){
+      singletonService = new SingletonService();
+    }
+
+    return singletonService;
+  }
 }
